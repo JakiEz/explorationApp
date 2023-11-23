8d713f5cf27220bc21a2353986481ca21bca1eb5
JakiEz <Jackietf4@gail.com>
2023-11-23 22:20:13 +0700

    link page

--- app/Home.js
+++ app/Home.js
@@ -1,9 +1,53 @@
 import React from 'react'
+import {View,Text, SafeAreaView, ScrollView, StyleSheet, FlatList} from 'react-native'
+const DATA = [
+
+  {
+    id:'1234',
+    title:'Jackie'
+  },
+  {
+    id:'12345',
+    title:'Game'
+  },
+  {
+    id:'123456',
+    title:'Phone'
+  },
+  {
+    id:'123467',
+    title:'Pro'
+  },
+]
+
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <SafeAreaView>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        <FlatList
+         data={DATA}
+         renderItem={({item})=>{
+          return(<View style={{width:300,backgroundColor:"green"}}><Text>{item.title}</Text></View>)
+         }}
+         >
+
+        </FlatList>
+ 
+     
+      </ScrollView>
+    </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  top:{
+    backgroundColor:"blue",
+    flexDirection:"row",
+    height:100,
+    width:100,
+    marginLeft:10
+  }
+})
 
 export default Home
