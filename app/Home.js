52540c578868c36279840bb0390c8cf2a8b161e8
JakiEz <Jackietf4@gail.com>
2023-12-17 23:42:58 +0700

    dwadwadwadwa

--- app/Home.js
+++ app/Home.js
@@ -13,23 +13,28 @@ import { Ionicons } from "@expo/vector-icons";
 import { router, Link } from "expo-router";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { useFonts } from "expo-font";
+import { Card, Title, Paragraph } from "react-native-paper";
 
 const DATA = [
   {
     id: "1234",
     title: "Jackie",
+    image: require("../pics/logo.png"),
   },
   {
     id: "12345",
     title: "Game",
+    image: require("../pics/phone.png"),
   },
   {
     id: "123456",
     title: "Phone",
+    image: require("../pics/pic.png"),
   },
   {
     id: "123467",
     title: "Pro",
+    image: require("../pics/propic.png"),
   },
 ];
 
@@ -61,9 +66,17 @@ const Home = () => {
             </Link>
           </TouchableOpacity> */}
       <View style={styles.p}>
-        <Text style={{ fontSize: 30, fontFamily: "Montserrat" }}>See what</Text>
+        <Text style={{ fontSize: 30 }}>See what</Text>
         <Text style={{ fontSize: 30 }}>your friends are doing</Text>
       </View>
+
+      <ScrollView horizontal style={{marginLeft:40,width:"80%",backgroundColor:"black",height:30,borderRadius:15}}>
+          {DATA.map((item, index) => (
+                <Text key={index} style={{alignSelf:"center",marginLeft:50, color:"white"}}>
+                  {item.id}
+                </Text>
+              ))}
+      </ScrollView>
 
       {/* <View style={{width:"100%", marginBottom:20}}>
         <FlatList
@@ -84,17 +97,21 @@ const Home = () => {
       </View> */}
 
       <FlatList
-        style={{height:600}}
+        style={{ height: 600 }}
         showsHorizontalScrollIndicator={false}
         vertical
         data={DATA}
         renderItem={({ item }) => {
+          console.log(item.image);
           return (
-            <TouchableOpacity style={styles.friends}>
-              <View style={{ justifyContent: "center", alignItems: "center" }}>
-                <Text style={{ textAlign: "center" }}>{item.title}</Text>
-              </View>
-            </TouchableOpacity>
+            <Card style={{ margin: 16, borderRadius: 15 }}>
+              <Card.Cover source={{uri: item.image}} />
+
+              <Card.Content>
+                <Title>Your Card Title</Title>
+                <Paragraph>Your card content goes here.</Paragraph>
+              </Card.Content>
+            </Card>
           );
         }}
       />
@@ -108,33 +125,33 @@ const styles = StyleSheet.create({
     height: 100,
     // backgroundColor: "blue",
     margin: 30,
-    
   },
   button: {
-    alignSelf: 'center',
-    justifyContent: 'center',
+    alignSelf: "center",
+    justifyContent: "center",
     marginTop: 30,
     width: 150,
     height: 30,
     borderRadius: 15,
-    backgroundColor: 'green',
+    backgroundColor: "green",
   },
   friends: {
-    marginLeft:0,
+    marginLeft: 0,
     borderRadius: 15,
     marginBottom: 10,
-    
-    alignSelf: "center",
-    height: 200,
-    width: 380,
+
+    alignSelf: "flex-start",
+    height: 50,
+    width: 50,
     backgroundColor: "green",
   },
 
   cat: {
-    width:8,                                                                                                              
-    height:20,
-    backgroundColor:"blue",
-    margin:20
+    width: "100%",
+    height: 20,
+    backgroundColor: "blue",
+    flexDirection: "row",
+    // margin:20
   },
 });
 
